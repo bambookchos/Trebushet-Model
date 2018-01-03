1aab4117dacb690f12d3bc57b6b3c9b9d9346504
Unknown <bambookchos@gmail.com>
2018-01-03 15:34:20 +0300

    Clean REPO for final update

--- face.h
+++
@@ -1,9 +0,0 @@
-#ifndef FACE_H
-#define FACE_H
-struct Face
-{
-    int point_num[3];
-    float light_intensive;
-};
-
-#endif // FACE_H
--- trebushet.cpp
+++
@@ -1,6 +0,0 @@
-#include "trebushet.h"
-
-trebushet::trebushet()
-{
-
-}
